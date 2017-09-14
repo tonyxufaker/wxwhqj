d8a4085243e998039643d0e573253a8ff2ae59d1
tony xu <xutao759153@126.com>
2017-09-14 22:58:28 +0800

    修改配置文件

--- page/views.py
+++ page/views.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader, Context
 
 # Create your views here.
 def index(request):
-    return render(request, 'templates/index.html')
+    template = loader.get_template('/var/www/whqj/page/templates/index.html')
+    return HttpResponse(template)
